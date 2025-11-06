6cc67f7ad30138b303cd609b9694f370f20cc62c
Lukas Poorthuis <lukas.poorthuis@posteo.de>
2025-11-06 13:55:26 +0100

    spy testing

--- src/app/books/dashboard-page/dashboard-page.spec.ts
+++ src/app/books/dashboard-page/dashboard-page.spec.ts
@@ -2,15 +2,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardPage } from './dashboard-page';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { Book } from '../shared/book';
+import { BookRatingHelper } from '../shared/book-rating-helper';
 
 describe('DashboardPage', () => {
   let component: DashboardPage;
   let fixture: ComponentFixture<DashboardPage>;
 
   beforeEach(async () => {
+
+    const ratingHelperMock = {
+      rateUp : (b: Book) => b,
+      rateDown : (b: Book) => b,
+    }
+
     await TestBed.configureTestingModule({
       imports: [DashboardPage],
-      providers: [provideZonelessChangeDetection()]
+      providers: [
+        provideZonelessChangeDetection(),
+        { provide: BookRatingHelper, useValue: ratingHelperMock},
+      ]
     })
     .compileComponents();
 
@@ -21,5 +32,17 @@ describe('DashboardPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('component.doRateUp() calls helper.rateUp()', () => {
+    const helper = TestBed.inject(BookRatingHelper);
+
+    spyOn(helper, 'rateUp').and.callThrough();
+
+    const testBook = { isbn: '123' } as Book;
+
+    component.doRateUp(testBook)
+
+    expect(helper.rateUp).toHaveBeenCalled()
   });
 });
